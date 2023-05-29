34a7551e4c32b93c6003314f9e31317957e0e33e
k88936 <1440978305@qq.com>
2023-05-29 16:03:29 +0000

    rewrite malloc add string

--- libc/include/stdlib.h
+++ libc/include/stdlib.h
@@ -2,6 +2,6 @@
 #include "libc.h"
 #ifndef STDLIC_H
 #define STDLIC_H
-
+typedef unsigned size_t;
 
 #endif
--- libc/string/string.c
+++ libc/string/string.c
@@ -101,23 +101,123 @@ int strcmp(register const char *s1, register const char *s2)
 
 	return (*((char *)s1) < *((char *)s2)) ? -1 : 1;
 
-}
-/*
-while (*((Wuchar *)s1) == *((Wuchar *)s2)) {
-		if (!*s1++) {
-			return 0;
-		}
-		++s2;
+}char *strncpy(char * __restrict s1, register const char * __restrict s2,
+				size_t n)
+{
+	register char *s = s1;
+
+	while (n) {
+		if ((*s = *s2) != 0) s2++; /* Need to fill tail with 0s. */
+		++s;
+		--n;
 	}
 
-	return (*((Wuchar *)s1) < *((Wuchar *)s2)) ? -1 : 1;
-#else
-	int r;
-
-	while (((r = ((int)(*((Wuchar *)s1))) - *((Wuchar *)s2++))
-			== 0) && *s1++);
-
-	return r;
-#endif
+	return s1;
 }
-*/
+
+size_t strcspn(const char *s1, const char *s2)
+{
+	register const char *s;
+	register const char *p;
+
+	for ( s=s1 ; *s ; s++ ) {
+		for ( p=s2 ; *p ; p++ ) {
+			if (*p == *s) goto done;
+		}
+	}
+ done:
+	return s - s1;
+}
+// char *strerror(int errnum)
+// {
+//     static char buf[_STRERROR_BUFSIZE];
+
+// 	__xpg_strerror_r(errnum, buf, sizeof(buf));
+
+// 	return buf;
+// }
+char *strpbrk(const char *s1, const char *s2)
+{
+	register const char *s;
+	register const char *p;
+
+	for ( s=s1 ; *s ; s++ ) {
+		for ( p=s2 ; *p ; p++ ) {
+			if (*p == *s) return (char *) s; /* silence the warning */
+		}
+	}
+	return NULL;
+}
+char *strrchr(register const  char *s, int c)
+{
+	register const char *p;
+
+	p = NULL;
+	do {
+		if (*s == (char) c) {
+			p = s;
+		}
+	} while (*s++);
+
+	return (char *) p;			/* silence the warning */
+}
+size_t strspn(const char *s1, const char *s2)
+{
+	register const char *s = s1;
+	register const char *p = s2;
+
+	while (*p) {
+		if (*p++ == *s) {
+			++s;
+			p = s2;
+		}
+	}
+	return s - s1;
+}
+char *strstr(const char *s1, const char *s2)
+{
+	register const char *s = s1;
+	register const char *p = s2;
+
+	do {
+		if (!*p) {
+			return (char *) s1;;
+		}
+		if (*p == *s) {
+			++p;
+			++s;
+		} else {
+			p = s2;
+			if (!*s) {
+				return NULL;
+			}
+			s = ++s1;
+		}
+	} while (1);
+}
+
+char *strtok_r(char * __restrict s1, const char * __restrict s2,
+				 char ** __restrict next_start)
+{
+	register char *s;
+	register char *p;
+
+
+	if (((s = s1) != NULL) || ((s = *next_start) != NULL)) {
+		if (*(s += strspn(s, s2))) {
+			if ((p = strpbrk(s, s2)) != NULL) {
+				*p++ = 0;
+			}
+		} else {
+			p = s = NULL;
+		}
+		*next_start = p;
+	}
+	return s;
+
+}
+char *strtok(char * __restrict s1, const char * __restrict s2)
+{
+	static char *next_start;	/* Initialized to 0 since in bss. */
+	return strtok_r(s1, s2, &next_start);
+}
